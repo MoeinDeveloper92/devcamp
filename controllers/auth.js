3811aedbf1f4429278e25df847e2ad5dcd9226e0
Moein Samani Nejad <moeinsamani1992@gmail.com>
2025-02-03 20:52:51 +0100

    implement register and login mechanism

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,56 @@ const asyncHandler = require("../middleware/async")
 //@route    POST api/v1/auth/register
 //@access   pivli
 exports.register = asyncHandler(async (req, res, next) => {
-    res.status(200).json({
-        success: true,
+    const { name, email, password, role } = req.body
+    //create user
+    const user = await User.create({
+        name,
+        email,
+        password,
+        role
     })
+
+    //keep in mind that, static is called on theModel , but method called on the created resource that we getting from db
+    //creat etoken
+    const token = user.getSignedJwtToken()
+    res.status(201).json({
+        success: true,
+        token
+    })
+
+})
+
+
+
+//@desc     login user
+//@route    POST /api/v1/auth/login
+//@access   Public
+exports.login = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body
+
+    //Validate email & password
+    if (!email || !password) {
+        return next(new ErrorResponse("Please provide an email and a password!", 400))
+    }
+    //check for the user
+    //+password means hey please add password, since in the model we excelude it
+    const user = await User.findOne({ email }).select("+password")
+
+    if (!user) {
+        return next(new ErrorResponse("Invalid Credentials!", 401))
+    }
+
+    //check if password matches
+    const isMatch = await user.matchPassword(password)
+
+    if (!isMatch) {
+        return next(new ErrorResponse("Invalid Credentials", 401))
+    }
+
+    //create Token
+    const token = user.getSignedJwtToken()
+
+    res.status(200).json({ success: true, token })
+
+
 })
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
-
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -31,6 +32,30 @@ const UserSchema = new mongoose.Schema({
     }
 })
 
+///bcryptjs
+UserSchema.pre("save", async function (next) {
+    //generaet a salt to use to hash the password
+    //the more salt, the secure password but it adds more load to the system to be decrpyed
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+})
 
+
+// Sign JWT and return
+//This i smethod and we call it on actual user
+//we can call this emthod within the contoller
+UserSchema.methods.getSignedJwtToken = function () {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE
+    })
+}
+
+
+//Match user entered passowrd to hashed passwor din DB
+//methods is called on the actude doc, which measn we have access to the fieldsby this
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
 const User = mongoose.model("User", UserSchema)
 module.exports = User
