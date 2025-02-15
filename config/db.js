4c25c4422d3615af1dee260d41930bb1389d1661
Moein Samani Nejad <moeinsamani1992@gmail.com>
2025-02-15 18:34:13 +0100

    cleaning code and ready for deployment

--- config/db.js
+++ config/db.js
@@ -1,14 +1,20 @@
 const mongoose = require("mongoose")
 
-const connectDB = async () => {
 
-    try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI, {})
-        console.log(`MongoDB Connected:${conn.connection.host}`.cyan.underline.bold)
-    } catch (error) {
-        console.log("Error=>>>", error)
-        process.exit(1)
+
+class MongoClient {
+    async connectDB() {
+        try {
+            const conn = await mongoose.connect(process.env.MONGODB_URI, {})
+            console.log(`MongoDB Connected:${conn.connection.host}`.cyan.underline.bold)
+        } catch (error) {
+            console.log("Error=>>>", error)
+            process.exit(1)
+        }
     }
 }
 
-module.exports = connectDB
+const mongoClient = new MongoClient()
+
+
+module.exports = mongoClient
--- server.js
+++ server.js
@@ -17,78 +17,80 @@ const dotenv = require("dotenv").config({
 })
 const errorHandler = require("./middleware/error")
 const colors = require("colors")
-const connectDB = require("./config/db")
-
-connectDB()
-//PORT
-const PORT = process.env.PORT || 5000
+const mongoClient = require("./config/db")
 
 
-const app = express()
 
-//add Body Parse
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
 
-//Cookie Parser
-app.use(cookieParse())
-//Dev Logging Middleware
-//I only want this to run if we are in development
-if (process.env.NODE_ENV === "development") {
-    app.use(morgan("dev"))
+
+const appServer = async () => {
+    await mongoClient.connectDB()
+    //PORT
+    const PORT = process.env.PORT || 5000
+
+
+    const app = express()
+
+    //add Body Parse
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: false }))
+
+    //Cookie Parser
+    app.use(cookieParse())
+    //Dev Logging Middleware
+    //I only want this to run if we are in development
+    if (process.env.NODE_ENV === "development") {
+        app.use(morgan("dev"))
+    }
+
+    //File Uploading
+    app.use(fileUpload())
+
+
+    //Set Securoty Headers
+    app.use(helmet())
+
+    //Prevent XSS attacks
+    app.use(xss())
+
+    //Sanitize Data
+    app.use(mongoSanitize())
+
+    //set static folder
+    app.use(express.static(path.join(__dirname, "public")))
+
+    //Routes
+    app.use("/api/v1/bootcamps", bootCampRoutes)
+    app.use("/api/v1/courses", coursRoutes)
+    app.use("/api/v1/auth", authRoutes)
+    app.use("/api/v1/users", userRoutes)
+    app.use("/api/v1/reviews", reviewRoutes)
+
+    app.use((req, res, next) => {
+        res.status(404).json({
+            message: 'Ohh you are lost, read the API documentation to find your way back home :)'
+        })
+    })
+
+    //Error Middleware
+    app.use(errorHandler)
+
+    const server = app.listen(PORT, "0.0.0.0", () => {
+        console.log(`Server running in ${process.env.NODE_ENV} mode on PORT:${PORT}`["yellow", "bold"])
+    })
+
+    //stop applcaiton if we get unhandlerd rejection
+    //Handler unhandled rejections or promise rejection
+    process.on("unhandledRejection", (err, promise) => {
+        console.log(`Error: ${err.message}`.red)
+        //Close Server & Exit Process
+        server.close(() => {
+            //code 1 means exit with failure!
+            process.exit(1)
+        })
+    })
 }
 
-//File Uploading
-app.use(fileUpload())
 
+appServer()
 
-//Set Securoty Headers
-app.use(helmet())
-
-//Prevent XSS attacks
-app.use(xss())
-
-//Sanitize Data
-app.use(mongoSanitize())
-
-//set static folder
-app.use(express.static(path.join(__dirname, "public")))
-
-//Routes
-app.use("/api/v1/bootcamps", bootCampRoutes)
-app.use("/api/v1/courses", coursRoutes)
-app.use("/api/v1/auth", authRoutes)
-app.use("/api/v1/users", userRoutes)
-app.use("/api/v1/reviews", reviewRoutes)
-
-app.use((req, res, next) => {
-    res.status(404).json({
-        message: 'Ohh you are lost, read the API documentation to find your way back home :)'
-    })
-})
-
-//Error Middleware
-app.use(errorHandler)
-
-const server = app.listen(PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV} mode on PORT:${PORT}`["yellow", "bold"])
-})
-
-
-//stop applcaiton if we get unhandlerd rejection
-//Handler unhandled rejections or promise rejection
-process.on("unhandledRejection", (err, promise) => {
-    console.log(`Error: ${err.message}`.red)
-    //Close Server & Exit Process
-    server.close(() => {
-        //code 1 means exit with failure!
-        process.exit(1)
-    })
-})
-
-
-
-
-////////////////////////////
-//in mongooe we have a poiece of middleware, when the user wants to create a bootcamp it uses geocoder, where it takes the actual addres and convers the address to geo locaiton, lat,lang. by
-//mapquest ... in mongoose we have geojson type
